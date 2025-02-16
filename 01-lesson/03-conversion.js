f4899ecc893ac44a975b5776e33971591fc19aca
Awais-op <Awaisyousaf479@gmail.com>
2025-02-16 08:10:05 +0000

    adding comment on typeof change

--- 01-lesson/03-conversion.js
+++ 01-lesson/03-conversion.js
@@ -12,4 +12,6 @@ let valueIsBoolean = Boolean(num)
 
 let Score = "1";
 let newNumberType = Number(Score)
-console.log(typeof newNumberType)
+//console.log(typeof newNumberType) // In here we checked th typeof Score
+
+
