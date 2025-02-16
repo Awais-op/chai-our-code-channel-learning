bb63dff17f536f4af2eb55d9b5f6b88adc31c81e
Awais-op <Awaisyousaf479@gmail.com>
2025-02-16 08:05:16 +0000

    Conversions

---
+++ 01-lesson/03-conversion.js
@@ -0,0 +1,15 @@
+let number = 1;
+let isBoolean = Boolean(number)
+//console.log(isBoolean) // Conversion of number 1 => true
+
+
+let num = 0;
+let valueIsBoolean = Boolean(num)
+//console.log(valueIsBoolean) // Conversion of num 0 => false
+
+
+// Now lets check the typeof above values 0 & 1 
+
+let Score = "1";
+let newNumberType = Number(Score)
+console.log(typeof newNumberType)
