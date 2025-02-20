1656ecabdf1c2775c0b163610657d6ccc4f92570
Awais-op <Awaisyousaf479@gmail.com>
2025-02-20 17:01:01 +0000

    in depth of dataTypes

---
+++ 01-lesson/01-basic.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 6;
+let c = a + b;
+const d = 10;
+
+
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.table([a,b,c,d])
+
+
---
+++ 01-lesson/02-dataType.js
@@ -0,0 +1,12 @@
+"strict" //for pushing to new version code
+
+let name = "Awais"; // string data
+let age = 22;        // number 
+accountLogin = true;  // Boolean data
+let object = null;
+let obj = Symbol.iterator;
+
+//console.log(typeof(null));
+console.log(typeof(Symbol))
+
+
